8f48a8e980d9769c26e5883153c9f544fed31261
Mike Decker <pookmish@gmail.com>
2017-04-07 10:25:36 -0700

    fixed duplicating icons

--- modules/stanford_icon_grid/js/stanford_icon_grid.js
+++ modules/stanford_icon_grid/js/stanford_icon_grid.js
@@ -7,13 +7,15 @@
   Drupal.behaviors.stanford_icon_grid = {
     attach: function (context, settings) {
       $('.stanford-fontawesome-icon', context).find('option').each(function () {
-        var icon = $('<i>', {
-          class: 'fa fa-' + $(this).attr('value')
-        });
-        $(this).prepend(icon);
+        if (!$(this).hasClass('icon-added')) {
+          var icon = $('<i>', {
+            class: 'fa fa-' + $(this).attr('value')
+          });
+          $(this).addClass('icon-added').prepend(icon);
+        }
       });
 
-      $('.field-name-field-s-icon-ext-columns-bg-colo option', context).each(function(){
+      $('.field-name-field-s-icon-ext-columns-bg-colo option', context).each(function () {
         var values = $(this).attr('value');
         values = values.split(',');
         $(this).css('background-color', values[0]);
