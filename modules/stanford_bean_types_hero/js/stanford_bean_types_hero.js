a4cc0bfc0fcc0109c14474d3a375a47c5cd9b593
Mike Decker <pookmish@gmail.com>
2017-05-25 14:43:28 -0700

    HSDO-554 Added scrolling and static view modes

--- modules/stanford_bean_types_hero/js/stanford_bean_types_hero.js
+++ modules/stanford_bean_types_hero/js/stanford_bean_types_hero.js
@@ -2,89 +2,77 @@
   Drupal.behaviors.stanfordBeanTypesHero = {
     attach: function (context, settings) {
 
-      // Move the hero to the top of the body tag.
-      $('.hero-curtain', context).each(function () {
-        var clonedBlock = $(this).detach().prependTo('body');
-        var wrapper = $('<div>', {class: 'hero-reveal'});
-        $(clonedBlock).siblings().wrapAll(wrapper);
+      var classes = ['hero-curtain', 'hero-static', 'hero-scroll'];
+      $.each(classes, function (i, heroClass) {
 
-        // If focus is moved away from the hero, scroll to the top of the normal page.
-        $(this).find('a').last().focusout(function (e) {
-          if ($(this).is(':visible')) {
-            var topPage = $(clonedBlock).height();
-            $('body').scrollTop(topPage);
-          }
-        })
+        // Move the hero to the top of the body tag.
+        $('.' + heroClass, context).each(function (i, a, b) {
+          var clonedBlock = $(this).detach().prependTo('body');
+          var wrapper = $('<div>', {class: heroClass + '-reveal'});
+          $(clonedBlock).siblings().wrapAll(wrapper);
+
+          // If focus is moved away from the hero, scroll to the top of the normal page.
+          $(this).find('a').last().focusout(function (e) {
+            if ($(this).is(':visible')) {
+              var topPage = $(clonedBlock).height();
+              $('body').scrollTop(topPage);
+            }
+          })
+        });
       });
 
-      var images = $('.hero-curtain img');
-
-      function heroCurtain() {
+      function heroSetSize() {
         var winHeight = $(window).height();
-        var winWidth = $(window).width();
+        $.each(classes, function (i, heroClass) {
+          var heroAssets = $('.' + heroClass)
+            .find('img, iframe');
 
-        var heroAssets = $('.hero-curtain')
-          .css('margin-bottom', $('.hero-reveal').height())
-          .find('img, iframe');
-
-        $(heroAssets).parent()
-          .height(winHeight)
-          .css('overflow', 'hidden');
-
-        $.each(images, function () {
-
-          var imgRatio = this.width / this.height;
-          var winRatio = winWidth / winHeight;
-
-          $(this).removeClass('stretch-vertical');
-          $(this).removeClass('stretch-horizontal');
-
-          // Taller than Wide Image
-          if (imgRatio < 0) {
-            if (imgRatio < winRatio) {
-              // Window has more narrow width and so image needs to stretch vertically.
-              $(this).addClass('stretch-vertical');
-            } else {
-              $(this).addClass('stretch-horizontal');
-            }
-          } else {
-            if (imgRatio < winRatio) {
-              // Window has more narrow width and so image needs to stretch horizontally.
-              $(this).addClass('stretch-horizontal');
-            } else {
-              $(this).addClass('stretch-vertical');
-            }
-          }
-
-          if ($(this).width() > winWidth) {
-            $(this).css('left', ($(this).width() - winWidth) / -2);
-          }
+          $(heroAssets).parent()
+            .height(winHeight)
+            .css('overflow', 'hidden');
         });
+        $('.hero-curtain').css('margin-bottom', $('.hero-curtain-reveal').height())
+      }
+
+      function heroScroll() {
+        $('.hero-scroll').append($('<a>', {
+          class: 'scroll-down',
+          href: '#',
+          title: Drupal.t('Scroll Down'),
+          'aria-label': Drupal.t('Scroll Down'),
+          html: '<div class="scroll-text">' + Drupal.t('Scroll') + '</div><div class="fa fa-arrow-circle-o-down"></div>'
+        }).click(function (e) {
+          e.preventDefault();
+          $("html, body").animate({scrollTop: $('.hero-scroll').height()}, 800);
+        }));
       }
 
       $(window).scroll(function (e) {
-        var curtain = $('.hero-curtain');
-        var scrollPos = 0 - $(curtain).height() + $(window).scrollTop();
+        $.each(classes, function (i, heroClass) {
+          var curtain = $('.' + heroClass);
+          var scrollPos = 0 - $(curtain).height() + $(window).scrollTop();
 
-        if (scrollPos < 0) {
-          $('.hero-reveal').removeClass('below-hero');
-          $(curtain).removeClass('below-hero');
-        } else {
-          $('.hero-reveal').addClass('below-hero');
-          $(curtain).addClass('below-hero');
-        }
+          if (scrollPos < 0) {
+            $('.' + heroClass + '-reveal').removeClass('below-hero');
+            $(curtain).removeClass('below-hero');
+          } else {
+            $('.' + heroClass + '-reveal').addClass('below-hero');
+            $(curtain).addClass('below-hero');
+          }
+        });
       });
-
-      $(window).resize(heroCurtain);
-      heroCurtain();
 
       $(window).load(function () {
         if (typeof $.imagesLoaded !== 'undefined') {
-          $('.hero-curtain').imagesLoaded(heroCurtain);
+          $('.hero-curtain').imagesLoaded(heroSetSize);
         } else {
-          heroCurtain();
+          heroSetSize();
         }
       });
+
+      $(window).resize(heroSetSize);
+      heroSetSize();
+      heroScroll();
     }
   }
 })(jQuery);
